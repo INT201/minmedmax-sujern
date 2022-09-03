1b7cf727c799311d30568cb62562b6d84e5d4bf1
sujern <sujern7856@gmail.com>
2022-09-04 02:22:31 +0700

    homework03

--- minMedMax.js
+++ minMedMax.js
@@ -1,6 +1,38 @@
 const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
-  //code here
+  const seekValue = [num1,num2,num3]
+
+    let maxValue = seekValue[0]
+    let minValue = seekValue[0]
+    let medValue = seekValue[0]
+
+    for(const value of seekValue){
+        if(value > maxValue){
+            maxValue = value;
+        }
+    }
+
+    for(const value of seekValue){
+        if(value < minValue){
+            minValue = value;
+        }
+    }
+
+    for(const value of seekValue){
+        if(value != minValue && value != maxValue){
+            medValue = value
+        }
+        if(num1 === num2){
+            medValue = num1
+        }
+        if(num1 === num3){
+        }
+        if(num2 === num3){
+            medValue = num2
+        }
+    }
+    
+    return {min: minValue, mid: medValue, max: maxValue}
 }
 module.exports = minMedMax
